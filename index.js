4e659a6f0ff612ad623f9a415c62ad96df1c9b16
luisdalopez56 <33089167+luisdalopez56@users.noreply.github.com>
2019-01-28 16:53:16 +0100

    Solucionado 'useNewUrlParser: true'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const config = require('./config')
 
-mongoose.connect(config.db, (err, res)  => {
+mongoose.connect(config.db, { useNewUrlParser: true } ,(err, res)  => {
     if (err) {
         return console.log(`Error al conectar a la base de datos ${err}`)
     }
